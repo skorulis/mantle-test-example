8b5b96918a0e92e60059ef05a75c38e15e4b8a73
Alexander Skorulis <skorulis@gmail.com>
2015-03-24 21:37:33 +1100

    Initial Commit

---
+++ mantle-test-example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mantle-test-example
+//
+//  Created by Alexander Skorulis on 24/03/2015.
+//  Copyright (c) 2015 com.skorulis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mantle-test-example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mantle-test-example
+//
+//  Created by Alexander Skorulis on 24/03/2015.
+//  Copyright (c) 2015 com.skorulis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
